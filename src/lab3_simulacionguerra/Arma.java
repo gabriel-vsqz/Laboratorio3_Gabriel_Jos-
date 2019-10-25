d912972b351914c9b03e65ac82d5d4c92083a00b
Gabriel Vásquez <gabevsqz04@gmail.com>
2019-10-25 13:53:30 -0600

    Creación de clase padre Arma e hijas

--- src/lab3_simulacionguerra/Arma.java
+++ src/lab3_simulacionguerra/Arma.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab3_simulacionguerra;
 
-/**
- *
- * @author Gabriel Vasquez
- */
 public class Arma {
+    
+    private String nombre;
+    private double precio;
+
+    public Arma() {
+    }
+
+    public Arma(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Arma{" + "nombre=" + nombre + ", precio=" + precio + '}';
+    }
     
 }
