feeacbe4ece515f871a11acb3e93a50d75bc32ba
Gabriel Vásquez <gabevsqz04@gmail.com>
2019-10-25 14:27:43 -0600

    Disposición del Menú

--- src/lab3_simulacionguerra/Lab3_SimulacionGuerra.java
+++ src/lab3_simulacionguerra/Lab3_SimulacionGuerra.java
@@ -19,6 +19,49 @@ public class Lab3_SimulacionGuerra {
                     + "7 - Modificar Ejército\n8 - Listar Ejército\n9 - Eliminar Ejército\n10 - Eliminar Soldad\n11 - Listar Soldados\n"
                     + "12 - Eliminar Arma\n13 - Listar Armas");
             option = input.nextInt();
+            
+            switch(option){
+                
+                case 1:
+                    break;
+                    
+                case 2:
+                    break;
+                    
+                case 3:
+                    break;
+                    
+                case 4:
+                    break;
+                    
+                case 5:
+                    break;
+                    
+                case 6:
+                    break;
+                    
+                case 7:
+                    break;
+                    
+                case 8:
+                    break;
+                    
+                case 9:
+                    break;
+                    
+                case 10:
+                    break;
+                    
+                case 11:
+                    break;
+                    
+                case 12:
+                    break;
+                
+                case 13:
+                    break;
+            }
+            
         }
     }
     
