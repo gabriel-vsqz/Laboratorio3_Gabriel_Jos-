3fb9fa36a6668c493d708ecdc5105dc0faf3743e
Gabriel Vásquez <gabevsqz04@gmail.com>
2019-10-25 15:26:27 -0600

    Trabajo en el menú ocpionde de arma

--- src/lab3_simulacionguerra/Lab3_SimulacionGuerra.java
+++ src/lab3_simulacionguerra/Lab3_SimulacionGuerra.java
@@ -8,12 +8,10 @@ public class Lab3_SimulacionGuerra {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
         
         ArrayList<Ejercito> ejercitos = new ArrayList();
         ArrayList<Arma> armas = new ArrayList();
-        
-
-        Scanner input = new Scanner(System.in);
         ArrayList<Soldado> soldados1 = new ArrayList();
 
         int option = 0;
@@ -23,7 +21,7 @@ public class Lab3_SimulacionGuerra {
             System.out.printf("\n%s", "1 - Crear Ejército\n2 - Crear Soldado\n3 - Agregar Soldado a un Ejército\n"
                     + "4 - Crear Arma\n4 - Comprar Arma para un Ejército\n5 - Asignar Arma a un Soldado\n6 - Modificar Ejército\n"
                     + "7 - Modificar Ejército\n8 - Listar Ejército\n9 - Eliminar Ejército\n10 - Eliminar Soldad\n11 - Listar Soldados\n"
-                    + "12 - Eliminar Arma\n13 - Listar Armas");
+                    + "12 - Eliminar Arma\n13 - Listar Armas\nIngrese opción que desea: ");
             option = input.nextInt();
 
             switch (option) {
@@ -40,7 +38,7 @@ public class Lab3_SimulacionGuerra {
                     break;
 
                 case 2: {
-
+                    
                     System.out.print("Ingrese el nombre del soldado: ");
                     nombre = input.nextLine();
                     System.out.print("Ingrese el lugar: ");
@@ -63,11 +61,56 @@ public class Lab3_SimulacionGuerra {
                 break;
 
                 case 4:
+                    
                     System.out.print("Ingrese el nombre del Arma: ");
                     String nombreA = input.next();
                     System.out.print("Ingrese el precio del Arma: ");
                     double price = input.nextDouble();
-                    System.out.print("Tipo de Arma");
+                    System.out.println("Tipo de Arma");
+                    System.out.printf("%s\n%s", "Tipo 1 - Bomba\nTipo 2 - Rifle\nTipo 3 - Arma Blanca",
+                            "Tipo deseado: ");
+                    int type = input.nextInt();
+                    while (type < 1 && type > 3) {
+                        System.out.println("El tipo que desea no existe, utilice los propuestos.");
+                        System.out.printf("%s\n%s", "Tipo 1 - Bomba\nTipo 2 - Rifle\nTipo 3 - Arma Blanca",
+                            "Tipo deseado: ");
+                        type = input.nextInt();
+                    }
+                    switch (type) {
+                        case 1:
+                            System.out.print("Material de la Bomba: ");
+                            String material = input.next();
+                            System.out.print("Alcance: ");
+                            int reach = input.nextInt();
+                            armas.add(new Bomba(material, reach, nombreA, price));
+                            break;
+                        case 2:
+                            System.out.print("Alcance Máximo: ");
+                            int max = input.nextInt();
+                            System.out.print("Alcance Mínimo: ");
+                            int min = input.nextInt();
+                            System.out.print("¿Es automático? [1 - Si/2 - No]: ");
+                            int auto = input.next().charAt(0);
+                            String autom = "";
+                            while(auto != 1 || auto != 2){
+                                System.out.print("Debe utilizar las opciones propuestas [1/2]");
+                                System.out.print("¿Es automático? [1 - Si/2 - No]: ");
+                                auto = input.next().charAt(0);
+                            }
+                            if (auto == 1) {
+                                autom = "Automática";
+                            }
+                            else {
+                                autom = "No automática";
+                            }
+                            armas.add(new Rifle(max, min, autom, nombreA, price));
+                            break;
+                        case 3:
+                            System.out.print("Material del Arma Blanca");
+                            material = input.next();
+                            armas.add(new ArmaBlanca(material, nombreA, price));
+                            break;
+                    }
                     
                     break;
 
@@ -81,6 +124,11 @@ public class Lab3_SimulacionGuerra {
                     break;
 
                 case 8:
+                    String salida = "";
+                    for (Ejercito t : ejercitos) {
+                        salida += ejercitos.indexOf(t) + " - " + t + "\n";
+                    }
+                    System.out.println(salida);
                     break;
 
                 case 9:
@@ -96,6 +144,11 @@ public class Lab3_SimulacionGuerra {
                     break;
 
                 case 13:
+                    salida = "";
+                    for (Arma t : armas) {
+                        salida += armas.indexOf(t) + " - " + t + "\n";
+                    }
+                    System.out.println(salida);
                     break;
             }
 
